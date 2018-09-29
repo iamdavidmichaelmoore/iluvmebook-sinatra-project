ea289c457cb807d4585c574cbf95b5183d805ac4
iamdavidmichaelmoore <hey@iamdavidmoore.com>
2018-09-29 18:01:35 -0400

    corrected add_column migration data type to be compatible with Postgresql

--- db/migrate/20180929165837_add_columns_awards.rb
+++ db/migrate/20180929165837_add_columns_awards.rb
@@ -3,6 +3,6 @@ class AddColumnsAwards < ActiveRecord::Migration
     add_column :awards, :issue_date, :string
     add_column :awards, :issuing_command_unit, :string
     add_column :awards, :location, :string
-    add_column :awards, :award_name, :blob
+    add_column :awards, :award_name, :text
   end
 end
